37a3875aa52511ed9f6d1385e567821ba163d5ea
Corey Mcmahon <contact@coreymcmahon.com>
2019-02-19 13:23:52 +0700

    [ch0] mocked shortlinker

--- functions/index.js
+++ functions/index.js
@@ -5,7 +5,9 @@ const base64url = require('base64url');
 
 // Constants and configuration
 const TOKEN_LENGTH = 9;
-const KEYS = process.env.KEYS ? process.env.KEYS.split(',') : [];
+const KEYS = process.env.KEYS ? process.env.KEYS.split(',') : [
+  'ABCDEFGHIJKLMNOPQRSTUVWXYZ', // @todo: remove
+];
 
 const randomToken = () => {
   return base64url(crypto.randomBytes(TOKEN_LENGTH));
@@ -15,8 +17,8 @@ const hash = {
   abcdefg: 'https://google.com',
   bbcdefg: 'https://amazon.com',
 };
-const get = (url) => {
-  return hash[`${key}`] || false; // @todo
+const get = (token) => {
+  return hash[`${token}`] || false; // @todo
 };
 const set = (url) => {
   const token = randomToken();
@@ -33,29 +35,30 @@ const fetchByToken = (token) => {
 };
 
 exports.get = functions.https.onRequest((req, res) => {
-  const url = fetchByToken();
+  const url = fetchByToken(req.url.substring(1));
   if (url) {
     res.redirect(url, 301);
   } else {
-    res.status(404);
-    res.write("Sorry, could not find that link.");
+    res.status(404)
+      .send("Sorry, could not find that link.");
   }
 });
 
 exports.post = functions.https.onRequest((req, res) => {
-  const key = req.headers["access-token"] || '';
-  const body = JSON.parse(req.body);
+  const key = (req.headers["authorization"] || '').replace('Bearer ', '');
+  const body = req.body;
 
   if (KEYS.indexOf(key) < 0) {
-    res.status(401);
-    res.write(JSON.stringify({message: "Unauthorized."}));
+    res.status(401)
+      .send(JSON.stringify({message: "Unauthorized."}));
   } else if (!body || !body.long_url) {
-    res.status(400);
-    res.write(JSON.stringify({message: "Invalid request format."}));
+    res.status(400)
+      .send(JSON.stringify({message: "Invalid request format."}));
   } else {
     const token = storeAsToken(body.long_url);
-    res.status(200);
-    res.write(JSON.stringify({link: `https://fkit.io/${token}`}));
+    res.status(200)
+      .header('content-type', 'application/json')
+      .send(JSON.stringify({link: `https://fkit.io/${token}`}));
   }
 });
 
@@ -66,7 +69,7 @@ exports.handle = functions.https.onRequest((req, res) => {
     case 'POST':
       return this.post(req, res);
     default:
-      res.status(404);
-      res.write("Sorry, could not find that link.");
+      res.status(404)
+        .send("Sorry, could not find that link.");
   }
 });
