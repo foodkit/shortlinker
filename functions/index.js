6668935a5baccba5ae5757a4749c4440c080e822
Corey Mcmahon <contact@coreymcmahon.com>
2019-02-20 10:05:23 +0700

    [ch0] finalize shortener

--- functions/index.js
+++ functions/index.js
@@ -1,75 +1,136 @@
 // Depedencies
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
 const crypto = require('crypto');
 const base64url = require('base64url');
+const validUrl = require('valid-url');
 
 // Constants and configuration
-const TOKEN_LENGTH = 9;
-const KEYS = process.env.KEYS ? process.env.KEYS.split(',') : [
-  'ABCDEFGHIJKLMNOPQRSTUVWXYZ', // @todo: remove
-];
+const TOKEN_LENGTH = 9; // bytes
+const KEYS = process.env.KEYS ? process.env.KEYS.split(',') : [];
+const HOSTNAME = process.env.HOSTNAME || 'https://fkit.io';
+const COLLECTION = process.env.COLLECTION || 'shortlinks';
+
+// Prepare Cloud Firestore
+admin.initializeApp(functions.config().firebase);
+const db = admin.firestore();
 
 const randomToken = () => {
   return base64url(crypto.randomBytes(TOKEN_LENGTH));
 };
 
-const hash = {
-  abcdefg: 'https://google.com',
-  bbcdefg: 'https://amazon.com',
-};
-const get = (token) => {
-  return hash[`${token}`] || false; // @todo
-};
-const set = (url) => {
-  const token = randomToken();
-  hash[token] = url; // @todo
-  return token;
-};
-
-const storeAsToken = (url) => {
-  return get(url) || set(url);
-};
-
-const fetchByToken = (token) => {
-  return get(token);
-};
-
-exports.get = functions.https.onRequest((req, res) => {
-  const url = fetchByToken(req.url.substring(1));
-  if (url) {
-    res.redirect(url, 301);
-  } else {
-    res.status(404)
-      .send("Sorry, could not find that link.");
+const validate = (url) => {
+  if (validUrl.isUri(url)) {
+    const currentHost = HOSTNAME.replace('http', '').replace('https', '');
+    const urlHost = url.replace('http', '').replace('https', '');
+    return !urlHost.startsWith(currentHost);
   }
+  return false;
+};
+
+/**
+ * Attempts to retrieve a shortlink from Firebase.
+ */
+const getShortlink = async (token) => {
+  console.log('getting doc', token);
+  return db.collection(COLLECTION)
+  .doc(token)
+  .get()
+  .then((doc) => {
+    console.log('doc', doc);
+    if (doc.exists) {
+      return doc.data().long_url;
+    }
+    return false;
+  })
+  .catch((err) => {
+    console.error(err);
+    return false;
+  });
+};
+
+/**
+ * Creates a shortlink document for the provided "long" URL.
+ */
+const setShortlink = async (url) => {
+  const token = randomToken();
+  return db.collection(COLLECTION)
+    .doc(token)
+    .set({long_url: url})
+    .then(() => {
+      return token;
+    })
+    .catch((err) => {
+      console.error(err);
+      return false;
+    });
+};
+
+/**
+ * GET /*
+ * Looks up a shortlink and redirects the request if the link was found.
+ */
+exports.get = functions.https.onRequest((req, res) => {
+  const token = req.url.substring(1);
+  getShortlink(token)
+    .then((longUrl) => {
+      if (longUrl) {
+        res.redirect(301, longUrl);
+      } else {
+        res.status(404)
+          .send('Sorry, could not find that link.');
+      }
+    }).catch((err) => {
+      console.error(err);
+      res.status(404)
+        .send('Sorry, could not find that link.');
+    });
 });
 
+/**
+ * POST /
+ * Creates a shortlink.
+ */
 exports.post = functions.https.onRequest((req, res) => {
-  const key = (req.headers["authorization"] || '').replace('Bearer ', '');
+  const key = (req.headers['authorization'] || '').replace('Bearer ', '');
   const body = req.body;
 
   if (KEYS.indexOf(key) < 0) {
+    // Authorization failed, invalid access token
     res.status(401)
-      .send(JSON.stringify({message: "Unauthorized."}));
-  } else if (!body || !body.long_url) {
-    res.status(400)
-      .send(JSON.stringify({message: "Invalid request format."}));
-  } else {
-    const token = storeAsToken(body.long_url);
-    res.status(200)
       .header('content-type', 'application/json')
-      .send(JSON.stringify({link: `https://fkit.io/${token}`}));
+      .send(JSON.stringify({message: 'Unauthorized.'}));
+  } else if (!body || !body.long_url || !validate(body.long_url)) {
+    // Request format is invalid
+    res.status(400)
+      .header('content-type', 'application/json')
+      .send(JSON.stringify({message: 'Invalid request format.'}));
+  } else {
+    // Create the shortlink:
+    setShortlink(body.long_url)
+    .then((token) => {
+      res.status(200)
+        .header('content-type', 'application/json')
+        .send(JSON.stringify({link: `${HOSTNAME}/${token}`}));
+    })
+    .catch((err) => {
+      console.error(err);
+    });
   }
 });
 
+/**
+ * Default request handler. Detects the HTTP method and sends the request to either
+ * GET or POST hanlder.
+ */
 exports.handle = functions.https.onRequest((req, res) => {
   switch (req.method) {
     case 'GET':
       return this.get(req, res);
     case 'POST':
       return this.post(req, res);
-    default:
-      res.status(404)
-        .send("Sorry, could not find that link.");
   }
+
+  res.status(404)
+    .send('Sorry, that method or path is not supported.');
 });
